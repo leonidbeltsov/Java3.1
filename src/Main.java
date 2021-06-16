c5dae6212959c1ad82e48420e7c99fb8c716c36c
Leonid Beltsov <leonidbeltsov@gmail.com>
2021-06-16 23:23:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 9987;
+        int bonusMilePrice = 20;
+        int bonus = ticketPrice / bonusMilePrice;
+        System.out.println(bonus);
+    }
+}
